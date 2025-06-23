ea006bd6f83c4e6c51ff48a676d6603bc8a8b64b
Miro Ostafiński <ostafinskim@gmail.com>
2025-06-23 16:13:31 +0100

    has selector

--- src/checkbox/checkbox.svelte
+++ src/checkbox/checkbox.svelte
@@ -2,7 +2,18 @@
   const { label = 'A Very Important Checkbox', id = 'checkbox', ...props } = $props();
 </script>
 
-<div>
-  <input {id} {...props} type="checkbox" />
-  <label for={id}>{label}</label>
+<div class="space-y-4">
+  <div
+    class="flex items-center gap-3 rounded-lg border p-4 has-[input:checked]:border-green-500 has-[input:checked]:bg-green-100"
+  >
+    <input type="checkbox" id="terms" class="peer h-4 w-4 accent-green-900" />
+    <label for="terms" class="">I agree to the terms</label>
+  </div>
+
+  <div
+    class="flex items-center gap-3 rounded-lg border p-4 has-[input:checked]:border-red-500 has-[input:checked]:bg-red-300"
+  >
+    <input type="checkbox" id="terms-no" class="peer h-4 w-4 accent-red-900" />
+    <label for="terms-no" class="">I do not agree to the terms</label>
+  </div>
 </div>
--- src/input/input.svelte
+++ src/input/input.svelte
@@ -4,10 +4,14 @@
   const errorId = error ? `${id}-error` : undefined;
 </script>
 
-<div>
-  <label for={id}>{label}</label>
+<div class="group space-y-1">
+  <label
+    class="block font-medium text-slate-900 after:ml-2 after:h-2 after:w-2 after:text-red-600 group-has-[input:required]:after:content-['*']"
+    for={id}>{label}</label
+  >
   <input
     {id}
+    class="block w-full rounded bg-white px-2 py-1 placeholder-slate-500 outline outline-slate-600 placeholder:font-light user-invalid:outline-red-600 focus:outline-2 focus:outline-blue-600"
     aria-describedby={descriptionId}
     aria-invalid={!!error}
     aria-errormessage={errorId}
